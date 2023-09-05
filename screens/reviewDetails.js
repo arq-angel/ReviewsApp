ab6285ad2ecf39cc7c8f9f98a6d3fad3922bc78c
Arjun Thapa Magar <arjun2057@hotmail.com>
2023-09-05 19:34:02 +1000

    complete Starting the Reviews App

---
+++ screens/reviewDetails.js
@@ -0,0 +1,17 @@
+import react from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+
+export default function ReviewDetails() {
+    return (
+        <View style={styles.container}>
+            <Text>ReviewDetails Screen</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        padding:24,
+    }
+})
